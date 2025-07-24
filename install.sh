a8a1877a48fe70b906939c87ea030f86c1c594aa
callum.zhong <callum.zhong@gmail.com>
2025-07-24 19:09:05 +0800

    update

--- install.sh
+++ install.sh
@@ -13,5 +13,10 @@ ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 # install Nix packages from config.nix
 nix-env -iA nixpkgs.myPackages
 
+# Set npm global prefix to avoid permission issues
+mkdir -p "$HOME"/.npm-global
+npm config set prefix ~/.npm-global
+echo 'export PATH=$PATH:~/.npm-global/bin' >>~/.bashrc
+
 # install aicommit2 via npm
 npm install -g aicommit2
