df76381f48ffc3e1c391c583459cf43421330cac
callum.zhong <callum.zhong@gmail.com>
2025-07-24 19:50:58 +0800

    update

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -4,6 +4,24 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
+vim.schedule(function()
+  local is_devcontainer = os.getenv 'REMOTE_CONTAINERS' or os.getenv 'CODESPACES' or os.getenv 'DEVCONTAINER' or vim.fn.filereadable '/.dockerenv' == 1
+
+  if is_devcontainer then
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+        ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+        ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+      },
+    }
+  end
+end)
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
