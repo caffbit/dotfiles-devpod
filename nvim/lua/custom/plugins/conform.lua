2bdb23c1d747a30012f348f2f31b28c52662c51c
callum.zhong <callum.zhong@gmail.com>
2025-07-24 17:10:22 +0800

    update

--- nvim/lua/custom/plugins/conform.lua
+++ nvim/lua/custom/plugins/conform.lua
@@ -3,19 +3,17 @@ return {
   'stevearc/conform.nvim',
   opts = {
     formatters_by_ft = {
-      -- JavaScript/TypeScript
-      javascript = { { 'prettierd', 'prettier' } },
-      typescript = { { 'prettierd', 'prettier' } },
-      javascriptreact = { { 'prettierd', 'prettier' } },
-      typescriptreact = { { 'prettierd', 'prettier' } },
-      -- Web
-      html = { { 'prettierd', 'prettier' } },
-      css = { { 'prettierd', 'prettier' } },
-      scss = { { 'prettierd', 'prettier' } },
-      json = { { 'prettierd', 'prettier' } },
-      jsonc = { { 'prettierd', 'prettier' } },
-      markdown = { { 'prettierd', 'prettier' } },
-      yaml = { { 'prettierd', 'prettier' } },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+      markdown = { 'prettierd', 'prettier', stop_after_first = true },
+      yaml = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
